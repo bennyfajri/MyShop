365705635473807a4bd9c820ee25a1930d21d9f5
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2023-11-27 21:36:25 +0700

    add : code scanner library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Barcode scanner
+    implementation("com.github.yuriy-budiyev:code-scanner:2.3.2")
 }
